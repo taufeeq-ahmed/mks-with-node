2e9514611f28e705ed303c3382d050380ad78a28
taufeeq-ahmed <themaestrotaufeeq@gmail.com>
2024-09-09 12:14:46 +0530

    routes are updated

--- catalog-service/src/routes/catalog.routes.ts
+++ catalog-service/src/routes/catalog.routes.ts
@@ -1,13 +1,94 @@
 import { NextFunction, Request, Response, Router } from "express";
+import { CatalogService } from "../services/catalog.service";
+import { CatalogRepository } from "../repository/product.repository";
+import { RequestValidator } from "../utils";
+import { CreateProductRequest, UpdateProductRequest } from "../dto/product.dto";
 
 const catalogRouter = Router();
 
-export const catalogService = {};
+export const catalogService = new CatalogService(new CatalogRepository());
 
+// endpoints
 catalogRouter.post(
-  "product",
+  "/products",
   async (req: Request, res: Response, next: NextFunction) => {
-    return;
+    try {
+      const { errors, input } = await RequestValidator(
+        CreateProductRequest,
+        req.body
+      );
+
+      if (errors) return res.status(400).json(errors);
+      const data = await catalogService.createProduct(input);
+      return res.status(201).json(data);
+    } catch (error) {
+      const err = error as Error;
+      return res.status(500).json(err.message);
+    }
+  }
+);
+
+catalogRouter.patch(
+  "/products/:id",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { errors, input } = await RequestValidator(
+        UpdateProductRequest,
+        req.body
+      );
+
+      const id = parseInt(req.params.id) || 0;
+
+      if (errors) return res.status(400).json(errors);
+
+      const data = await catalogService.updateProduct({ id, ...input });
+      return res.status(200).json(data);
+    } catch (error) {
+      const err = error as Error;
+      return res.status(500).json(err.message);
+    }
+  }
+);
+
+catalogRouter.get(
+  "/products",
+  async (req: Request, res: Response, next: NextFunction) => {
+    const limit = Number(req.query["limit"]);
+    const offset = Number(req.query["offset"]);
+    try {
+      const data = await catalogService.getProducts(limit, offset);
+      return res.status(200).json(data);
+    } catch (error) {
+      const err = error as Error;
+      return res.status(500).json(err.message);
+    }
+  }
+);
+
+catalogRouter.get(
+  "/products/:id",
+  async (req: Request, res: Response, next: NextFunction) => {
+    const id = parseInt(req.params.id) || 0;
+    try {
+      const data = await catalogService.getProduct(id);
+      return res.status(200).json(data);
+    } catch (error) {
+      return next(error);
+    }
+  }
+);
+
+catalogRouter.delete(
+  "/products/:id",
+  async (req: Request, res: Response, next: NextFunction) => {
+    const id = parseInt(req.params.id) || 0;
+    try {
+      const data = await catalogService.deleteProduct(id);
+      return res.status(200).json(data);
+    } catch (error) {
+      const err = error as Error;
+      return res.status(500).json(err.message);
+    }
   }
 );
 
